35cc90b9569f78800e3d36c415b0f799e1ea2848
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2017-08-28 23:55:55 -0300

    Add initial files: basic pokemon and moves search

---
+++ js/addPokemonController.js
@@ -0,0 +1,47 @@
+pokeApp.controller('addPokemon', function($scope, $http){
+	$scope.pokemonModel = {
+	name: '',
+	selectedName: '',
+	move1: '',
+	selectedMove1: '',
+	move2: '',
+	selectedMove2: '',
+	move3: '',
+	selectedMove3: '',
+	move4: '',
+	selectedMove4: ''
+	};
+	$scope.selectPokemon = function(pokemon){
+	$scope.pokemonModel.selectedName = pokemon;
+	$scope.pokemonModel.name = pokemon;
+	}
+	$scope.selectMove1 = function(m){
+		$scope.pokemonModel.selectedMove1 = m;
+	}
+	$scope.selectMove2 = function(m){
+		$scope.pokemonModel.selectedMove2 = m;
+	}
+	$scope.selectMove3 = function(m){
+		$scope.pokemonModel.selectedMove3 = m;
+	}
+	$scope.selectMove4 = function(m){
+		$scope.pokemonModel.selectedMove4 = m;
+	}
+	$http.get("http://pokeapi.co/api/v2/pokemon/?limit=900")
+	.then(function(response){
+		$scope.listPokemons = response.data;
+	},
+	function(response){
+		$scope.apiError='Erro ao conectar a API POKEAPI, tente atualizar a página';
+	});
+
+	$http.get("http://pokeapi.co/api/v2/move/?limit=700")
+	.then(function(response){
+		$scope.listMoves = response.data;
+	},
+	function(response){
+		$scope.apiError='Erro ao conectar a API POKEAPI, tente atualizar a página';
+	});
+
+
+});
